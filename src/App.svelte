aa3826268bc8344c242409b9a9133f0cdd4e4579
nickshouse <nickshouse@proton.me>
2023-07-26 01:10:18 +0000

    Jul 25, 2023, 8:10 PM

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let name: string = 'Alex';  // replace 'Alex' with your name
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <p>Welcome to my Svelte and TypeScript website.</p>
+</main>
