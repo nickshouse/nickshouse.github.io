2a83e96cd64fd77fb1eb68fe97f9158e70d1fa09
nickshouse <nickshouse@proton.me>
2023-07-27 15:33:22 -0500

    Add icons and align project titles to the right in project links
    Add icons and align project titles to the right in project links for better visual representation.

--- main.js
+++ main.js
@@ -3,22 +3,26 @@ window.onload = function() {
         { 
             name: "DisHub", 
             link: "https://github.com/nickshouse/githubdiscord",
-            description: "This is a project about integrating GitHub with Discord."
+            description: "This is a project about integrating GitHub with Discord.",
+            icon: "fas fa-code"
         },
         { 
             name: "Amazon Bot", 
             link: "https://github.com/nickshouse/Amazon-Bot",
-            description: "A bot to automate certain tasks on Amazon."
+            description: "A bot to automate certain tasks on Amazon.",
+            icon: "fas fa-robot"
         },
         { 
             name: "Windows Stuff", 
             link: "https://github.com/nickshouse/Windows-Stuff",
-            description: "A collection of scripts for managing Windows systems."
+            description: "A collection of scripts for managing Windows systems.",
+            icon: "fas fa-windows"
         },
         { 
             name: "Chao Bot", 
             link: "https://github.com/nickshouse/Chao-Bot",
-            description: "A Bot for Discord that simulates raising Chao from Sonic games."
+            description: "A Bot for Discord that simulates raising Chao from Sonic games.",
+            icon: "fas fa-gamepad"
         } 
     ];
 
@@ -29,7 +33,7 @@ window.onload = function() {
         projectElement.className = 'project';
         projectElement.innerHTML = `
             <a class="project-link" href="${project.link}" target="_blank">
-                <h2 class="project-title">${project.name}</h2>
+                <h2 class="project-title">${project.name} <i class="${project.icon} icon-right"></i></h2>
                 <p>${project.description}</p>
             </a>
         `;
