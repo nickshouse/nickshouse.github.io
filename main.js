ed1ce84c2608f9a0b9952a2c5e00c9f37632855f
nickshouse <nickshouse@proton.me>
2023-07-27 17:53:21 -0500

    Update footer text and add icon to project titles.
    - Footer text in index.html was updated to display a different username.
    - A new icon was added to each project title in main.js.
    - Minor CSS changes were made in styles.css to improve hover effects.

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-window.onload = function() {
+document.addEventListener('DOMContentLoaded', function() {
+    populateProjects();
+    setupHamburgerMenu();
+    setupThemeToggle();
+});
+
+function populateProjects() {
     const projects = [
         { 
             name: "DisHub", 
-            link: "https://github.com/nickshouse/githubdiscord",
-            description: "This is a project about integrating GitHub with Discord."
+            link: "https://github.com/nickshouse/DisHub",
+            description: "This is a Discord bot that makes it possible to monitor the commits of a specified GitHub repository. The bot will post each commit to a specified Discord channel in real time.",
+            icon: "fas fa-code"
         },
         { 
             name: "Amazon Bot", 
             link: "https://github.com/nickshouse/Amazon-Bot",
-            description: "A bot to automate certain tasks on Amazon."
+            description: "This is an unfinished automation bot.",
+            icon: "fas fa-robot"
         },
         { 
             name: "Windows Stuff", 
             link: "https://github.com/nickshouse/Windows-Stuff",
-            description: "A collection of scripts for managing Windows systems."
+            description: "A collection of scripts for managing Windows systems.",
+            icon: "fas fa-windows"
         },
         { 
             name: "Chao Bot", 
             link: "https://github.com/nickshouse/Chao-Bot",
-            description: "A Bot for Discord that simulates raising Chao from Sonic games."
+            description: "This is a Discord bot that simulates raising Chao from the Sonic Adventure games. It is a large scoped project that is currently a work in progress.",
+            icon: "fas fa-gamepad"
         } 
     ];
 
@@ -29,34 +39,36 @@ window.onload = function() {
         projectElement.className = 'project';
         projectElement.innerHTML = `
             <a class="project-link" href="${project.link}" target="_blank">
-                <h2 class="project-title">${project.name}</h2>
+                <h2 class="project-title">${project.name} <i class="${project.icon} icon-right"></i></h2>
                 <p>${project.description}</p>
             </a>
         `;
         projectsContainer.appendChild(projectElement);
     }
+}
 
+function setupHamburgerMenu() {
     const hamburger = document.getElementById('hamburger');
     const sidebar = document.getElementById('sidebar');
 
     hamburger.addEventListener('click', function() {
         sidebar.classList.toggle('active');
     });
+}
 
-    document.addEventListener('click', function(event) {
-        if (!sidebar.contains(event.target) && !hamburger.contains(event.target)) {
-            sidebar.classList.remove('active');
-        }
-    });
-
-    // Add theme toggle functionality
+function setupThemeToggle() {
     const themeToggle = document.getElementById('checkbox');
 
+    // Load the stored theme
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+        document.documentElement.setAttribute('data-theme', storedTheme);
+        themeToggle.checked = (storedTheme === 'dark');
+    }
+
     themeToggle.addEventListener('change', function() {
-        if (this.checked) {
-            document.documentElement.setAttribute('data-theme', 'dark');
-        } else {
-            document.documentElement.setAttribute('data-theme', 'light');
-        }
+        const theme = this.checked ? 'dark' : 'light';
+        document.documentElement.setAttribute('data-theme', theme);
+        localStorage.setItem('theme', theme);
     });
-};
+}
